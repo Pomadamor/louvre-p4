c6dff5d9e4b134571b97b1c9165173ed1f0a4b81
Lerna Ines <eck.ines@gmail.com>
2017-11-14 15:34:41 +0100

    Revu de 0 a partir du cour des controller vue et route, modification pour projet a venir.

--- src/BilletBundle/Controller/BilletController.php
+++ src/BilletBundle/Controller/BilletController.php
@@ -3,48 +3,120 @@
 namespace BilletBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use BilletBundle\Entity\billet;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+// use BilletBundle\Entity\billet;
+// use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+// use Symfony\Component\Form\Extension\Core\Type\DateType;
+// use Symfony\Component\Form\Extension\Core\Type\FormType;
+// use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+// use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+// use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BilletController extends Controller
 {
-    public function indexAction()
+
+  public function indexAction()
     {
-        return $this->render('BilletBundle:Billet:home.html.twig');
-	//return new Response($content);
+      // ...
+      // Notre liste d'annonce en dur
+      $listBillets = array(
+        array(
+          'title'   => 'Recherche développpeur Symfony',
+          'id'      => 1,
+          'author'  => 'Alexandre',
+          'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+          'date'    => new \Datetime()),
+        array(
+          'title'   => 'Mission de webmaster',
+          'id'      => 2,
+          'author'  => 'Hugo',
+          'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+          'date'    => new \Datetime()),
+        array(
+          'title'   => 'Offre de stage webdesigner',
+          'id'      => 3,
+          'author'  => 'Mathieu',
+          'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+          'date'    => new \Datetime())
+      );
+      // Et modifiez le 2nd argument pour injecter notre liste
+      return $this->render('BilletBundle:Billet:index.html.twig', array(
+        'listBillets' => $listBillets
+      ));
     }
-    public function addAction()
-    {
-    // On crée un objet billet et commande
-    $billet = new billet();
 
-    // On crée le FormBuilder grâce au service form factory
-    $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $billet);
-
-    // On ajoute les champs de l'entité que l'on veut à notre formulaire
-    $formBuilder
-      ->add('nom',     TextType::class)
-      ->add('prenom',     TextType::class)
-      ->add('journee', CheckboxType::class)
-      ->add('save',      SubmitType::class)
-    ;
-    // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
-
-    // À partir du formBuilder, on génère le formulaire
-    $form = $formBuilder->getForm();
-
-    // On passe la méthode createView() du formulaire à la vue
-    // afin qu'elle puisse afficher le formulaire toute seule
-    return $this->render('BilletBundle:Billet:index.html.twig', array(
-      'form' => $form->createView(),
+  public function viewAction($id)
+  {
+    $billet = array(
+      'title'   => 'Recherche développpeur Symfony2',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime()
+    );
+    return $this->render('BilletBundle:Billet:view.html.twig', array(
+      'billet' => $billet
     ));
   }
+
+  public function addAction(Request $request)
+  {
+    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+    if ($request->isMethod('POST')) {
+      // Ici, on s'occupera de la création et de la gestion du formulaire
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+      // Puis on redirige vers la page de visualisation de cettte annonce
+      return $this->redirectToRoute('Billet_view', array('id' => 5));
+    }
+    // Si on n'est pas en POST, alors on affiche le formulaire
+    return $this->render('BilletBundle:Billet:add.html.twig');
+  }
+  public function editAction($id, Request $request)
+  {
+    // Ici, on récupérera l'annonce correspondante à $id
+    // Même mécanisme que pour l'ajout
+    if ($request->isMethod('POST')) {
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+      return $this->redirectToRoute('Billet_view', array('id' => 5));
+    }
+    return $this->render('BilletBundle:Billet:edit.html.twig');
+  }
+
+  public function deleteAction($id)
+  {
+    // Ici, on récupérera l'annonce correspondant à $id
+    // Ici, on gérera la suppression de l'annonce en question
+    return $this->render('BilletBundle:Billet:delete.html.twig');
+  }
+
+  //   public function addAction()
+  //   {
+  //   // On crée un objet billet et commande
+  //   $billet = new billet();
+  //
+  //   // On crée le FormBuilder grâce au service form factory
+  //   $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $billet);
+  //
+  //   // On ajoute les champs de l'entité que l'on veut à notre formulaire
+  //   $formBuilder
+  //     ->add('nom',     TextType::class)
+  //     ->add('prenom',     TextType::class)
+  //     ->add('journee', CheckboxType::class)
+  //     ->add('save',      SubmitType::class)
+  //   ;
+  //   // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
+  //
+  //   // À partir du formBuilder, on génère le formulaire
+  //   $form = $formBuilder->getForm();
+  //
+  //   // On passe la méthode createView() du formulaire à la vue
+  //   // afin qu'elle puisse afficher le formulaire toute seule
+  //   return $this->render('BilletBundle:Billet:index.html.twig', array(
+  //     'form' => $form->createView(),
+  //   ));
+  // }
 
   //   public function addCommandeAction()
   //   {
--- src/OC/platformBundle/OCplatformBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OC\platformBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class OCplatformBundle extends Bundle
-{
-}
